42c369cabfa6d1f89a1bf6ad919704478f84d214
sophieAdlen <144331073+sophieAdlen@users.noreply.github.com>
2024-04-30 10:54:05 +0200

    create recipe

--- src/components/pages/Recipe.jsx
+++ src/components/pages/Recipe.jsx
@@ -1,6 +1,12 @@
+import './Recipe.css';
+
 function Recipe() {
     return (
-<p>recipe</p>    )
+        <div className="recipe-container">
+        <div className="square"></div>
+        <button className="create-button">Create Recipe</button>
+      </div>
+  )
 }
 
 export default Recipe
