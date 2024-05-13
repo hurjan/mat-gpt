128cec6d5da3a1aa4fea71bd7e85251e53ecdcc8
sophieAdlen <144331073+sophieAdlen@users.noreply.github.com>
2024-05-13 16:51:56 +0200

    arrow button

--- src/components/pages/Allergies.jsx
+++ src/components/pages/Allergies.jsx
@@ -5,12 +5,15 @@ import "../styles/Diet.css"
 
 function Allergies() {
   return (
+    <>
+    <a href="/diet"><button className="ArrowButton">&lt;</button></a>
     <div className="dietContainer">
       <p>Do you have any allergy /
         <br /> something you don't like?</p>
        <AllergiesButtons/>  
       <a href="/CreateRecipe"><button className="nextButton">Next</button></a>
     </div>
+    </>
   );
 }
 
--- src/components/pages/Diet.jsx
+++ src/components/pages/Diet.jsx
@@ -5,11 +5,14 @@ import "../styles/Diet.css"
 
 function Diet() {
   return (
+    <>
+    <a href="/"><button className="ArrowButton">&lt;</button></a>
     <div className="dietContainer">
       <p>Do you have any special diet ?</p>
        <DietButtons/>  
       <a href="/Allergies"><button className="nextButton">Next</button></a>
     </div>
+    </>
   );
 }
 
