17cb5161f9fb80f6781049db28d12707dd05f7f6
sophieAdlen <144331073+sophieAdlen@users.noreply.github.com>
2024-05-17 14:11:37 +0200

    fix

--- src/components/pages/Diet.jsx
+++ src/components/pages/Diet.jsx
@@ -2,35 +2,34 @@
 import DietButtons from "../DietButtons"
 import "../styles/Diet.css"
 
-import React, { useState } from 'react';
-
 
 function Diet() {
-  const [selectedDiets, setSelectedDiets] = useState([]);
-
-  const handleButtonClick = (diet) => {
-    if (selectedDiets.includes(diet)) {
-      setSelectedDiets(selectedDiets.filter(item => item !== diet));
-    } else {
-      setSelectedDiets([...selectedDiets, diet]);
-    }
-  };
-
   return (
     <>
-      <a href="/"><button className="ArrowButton">&lt;</button></a>
-      <div className="dietContainer">
-        <p>Do you have any special diets?</p>
-        <div className="selectedContainer">
-          {selectedDiets.map((diet, index) => (
-            <p key={index} className="selectedDiet">{diet}</p>
-          ))}
-        </div>
-        <DietButtons onButtonClick={handleButtonClick} />
-        <a href="/Allergies"><button className="nextButton">Next</button></a>
-      </div>
+    <a href="/"><button className="ArrowButton">&lt;</button></a>
+    <div className="dietContainer">
+      <p>Do you have any special diet ?</p>
+       <DietButtons/>  
+      <a href="/Allergies"><button className="nextButton">Next</button></a>
+    </div>
     </>
   );
 }
 
 export default Diet;
+
+
+// function Diet() {
+//   return (
+//     <>
+//     <a href="/"><button className="ArrowButton">&lt;</button></a>
+//     <div className="dietContainer">
+//       <p>Do you have any special diet ?</p>
+//        <DietButtons/>  
+//       <a href="/Allergies"><button className="nextButton">Next</button></a>
+//     </div>
+//     </>
+//   );
+// }
+
+// export default Diet;
