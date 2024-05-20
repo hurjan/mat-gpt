e40d522ef8ea8d694635302708ed84d253dd60e5
simonlundmark1 <155955074+simonlundmark1@users.noreply.github.com>
2024-05-20 18:27:45 +0200

    Create recipe

---
+++ src/components/CreateRecipeButtons.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import '../components/styles/Buttons.css';
+
+function CreateRecipeButtons({ onCookingMethodSelect }) {
+  // Funktion för att hantera när en knapp klickas och skicka tillbaka den valda Cooking method
+  const handleClick = (CookingMethod) => {
+    onCookingMethodselect(CookingMethod)
+  };
+
+  return (
+    <div className="button-container">
+      <div className="buttoncontent">
+        <button className="allergieButton" onClick={() => handleClick('Oven')}>
+          <img src={'/images/oven.png'} alt="Oven image" />
+          <span className="button-text">oven</span>
+        </button>
+      </div>
+      <div className="buttoncontent">
+      <button className="allergieButton" onClick={() => handleClick('Stove')}>
+          <img src={'/images/stove.png'} alt="Stove image" />
+          <span className="button-text">stove</span>        </button>
+      </div>
+      <div className="buttoncontent">
+        <button className="allergieButton" onClick={() => handleClick('Micro')}>
+        <img src={'/images/micro.png'} alt="Micro image" />
+          <span className="button-text">micro</span>
+        </button>
+      </div>
+      <div className="buttoncontent">
+        <button className="allergieButton" onClick={() => handleClick('Blender')}>
+          <img src={'/images/blender.png'} alt="blender image" />
+          <span className="button-text">blender</span>
+        </button>
+         </div>
+    </div>
+  );
+}
+
+export default CreateRecipeButtons;
--- src/components/pages/CreateRecipe.jsx
+++ src/components/pages/CreateRecipe.jsx
@@ -1,12 +1,20 @@
 import "../styles/CreateRecipe.css"
+import CreateRecipeButtons from "../CreateRecipeButtons";
 
 function CreateRecipe() {
     return (
         <div className="recipe-container">
-        <div className="square"></div>
-        <button className="create-button">Create Recipe</button>
+          <p>Create Recipe</p>
+          <p>Cooking method</p>
+          <CreateRecipeButtons/>  
+        <p> Cooking time</p>
+        
+        <p>Servings</p>
+{/*         <div className="square"></div>
+ */}        <button className="create-button">Create Recipe</button>
       </div>
   )
 }
+
 
 export default CreateRecipe;
