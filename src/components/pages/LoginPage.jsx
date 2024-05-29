240c0562c7978808e84a55fe29c1b52ae7ea6248
jakob-mbi <jakob_mbi@msn.com>
2024-05-29 03:16:02 +0200

    feat sign up validation

--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -1,14 +1,14 @@
 import "../styles/LoginPage.css"
 import {Link} from "react-router-dom";
 import {useForm} from "react-hook-form"
-//import logo from "../../../public/images/logo.png"
 import { DevTool } from "@hookform/devtools"
 
 
 
 function LoginPage() {
-    const form= useForm()
-    const {register,control,handleSubmit} = form
+    const form= useForm();
+    const {register,control,handleSubmit,formState} = form;
+    const {errors}= formState;
     const onSubmit = (data) => {
         console.log('Form submitted',data)
     }
@@ -26,13 +26,14 @@ function LoginPage() {
                             <input type="email" name="email" id="email" placeholder="name@email.com" 
                             className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                             {...register("email",{required:'Email is required'})}/>
-                            
+                            <p className="error">{errors.email?.message}</p>
                         </div>
                         <div>
                             <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
                             <input type="password" name="password" id="password" placeholder="••••••••" 
                             className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
-                            required=""{...register("password",{required:'Password is required'})}/>
+                            {...register("password",{required:'Password is required'})}/>
+                             <p className="error">{errors.password?.message}</p>
                         </div>
                         <button type="submit" className="w-full text-white bg-emerald-500 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login</button>
                         <p className="text-sm font-light text-gray-500 dark:text-gray-400">
