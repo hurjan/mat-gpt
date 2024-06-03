e49084dc8a4d47626b7dcd8348e7cf30fd7c61c1
Hurjan <70528122+hurjan@users.noreply.github.com>
2024-06-03 14:35:26 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,14 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()]
+  preview: {
+    port: 8080,
+    strictport: true,
+},
+server: {
+ port: 8080,
+ strictport: true,
+ host: true,
+ origin: "http://0.0.0.0:8000",
+},      
 });
